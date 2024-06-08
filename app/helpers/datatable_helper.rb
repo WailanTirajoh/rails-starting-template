077b5730f0f9267778eaf55e5e5c2bfcee169389
Wailan Tirajoh <wailantirajoh@gmail.com>
2024-06-09 02:19:14 +0700

    feat(posts): sortable datatable

---
+++ app/helpers/datatable_helper.rb
@@ -0,0 +1,44 @@
+module DatatableHelper
+  def sortable_th(options = {})
+    sortable = options.fetch(:sortable, true)
+
+    content_tag(:th, class: options[:class_style]) do
+      if sortable
+        sortable_column(options[:label], options[:column], options[:path])
+      else
+        concat options[:label]
+      end
+    end
+  end
+
+  def sortable_column(label, column, path)
+    direction = column.to_s == params[:sort].to_s && params[:direction] == 'asc' ? 'desc' : 'asc'
+    query_params = request.query_parameters.merge(sort: column, direction:)
+    path = send(path, query_params)
+
+    link_to(path, class: 'flex items-center justify-between') do
+      concat label
+      concat sort_icon(column)
+    end
+  end
+
+  def sort_icon(column)
+    return unless params[:sort].to_s == column.to_s
+
+    if params[:direction] == 'asc'
+      svg_icon('M5 15l7-7 7 7')
+    else
+      svg_icon('M19 9l-7 7-7-7')
+    end
+  end
+
+  def svg_icon(path_d)
+    content_tag(:svg, xmlns: 'http://www.w3.org/2000/svg',
+                      class: 'ml-1 inline w-4 h-4',
+                      fill: 'none',
+                      viewBox: '0 0 24 24',
+                      stroke: 'currentColor') do
+      "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='#{path_d}'></path>".html_safe
+    end
+  end
+end

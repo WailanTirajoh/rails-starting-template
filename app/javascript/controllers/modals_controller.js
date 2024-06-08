5a943b3a7806c11f546fb0fbfa664fde5b623a02
Wailan Tirajoh <wailantirajoh@gmail.com>
2024-06-09 00:57:45 +0700

    feat(post): update ui using table, create & edit using modal

--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -2,7 +2,13 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modals"
 export default class extends Controller {
-  connect() {}
+  connect() {
+    document.addEventListener("modal-close", this.closeModal.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener("modal-close", this.closeModal.bind(this));
+  }
 
   close(e) {
     e.preventDefault();
@@ -11,5 +17,9 @@ export default class extends Controller {
     modal.innerHTML = "";
     modal.removeAttribute("src");
     modal.removeAttribute("complete");
+  }
+
+  closeModal() {
+    this.close(new Event("close"));
   }
 }
